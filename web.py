34e834ba0b99e5e1205bba2da00d16cb3f490ffc
baonq-me <quocbao747@gmail.com>
2018-06-05 21:46:26 +0700

    Adding api gateway

--- web.py
+++ web.py
@@ -8,14 +8,13 @@ from bgn import BGN
 import argparse
 
 
-class MainHandler(tornado.web.RequestHandler):
+class Genkey(tornado.web.RequestHandler):
     def get(self):
     	#self.write(json.dumps(self.request.body))
         #self.write("Hi Sandeep, Welcome to Tornado Web Framework.")
         pkey,skey = BGN(8).genKey()
 
         self.write(json.dumps({"pkey":pkey,"skey":skey}))
-
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = 'BGN Web API')
@@ -24,7 +23,7 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	application = tornado.web.Application([
-		(r"/genkey", MainHandler),
+		(r"/genkey", Genkey),
 	])
 
 	application.listen(args.port[0])
